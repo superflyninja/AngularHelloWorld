91a93d58a1c1e97ac68a177881f236a68357858e
Patrick Horgan <phn@ersl.ie>
2018-07-27 09:57:56 +0100

    Bout halfway through the tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -14,7 +17,9 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger,style,transition,animate,keyframes,query,stagger} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('items', [
+      transition('* => *', [
+        query(':enter',style({opacity: 0}),{optional: true}),
+        query(':enter',stagger('300ms',[
+          animate('.6s ease-in',keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+          ]))
+        ]),{optional: true})
+      ])
+    ])
+  ]
 })
+
 export class HomeComponent implements OnInit {
+
+  itemCount: number = 4;
+
+  btnText: string = 'Add item';
+  itemListText: string = 'My first list item';
+  items = ["Item 1", "Item 2", "Item 3"];
 
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.items.length;
+    
+  }
+
+  addItem(){
+    this.items.push(this.itemListText);
+    this.itemListText ='';
+    this.itemCount = this.items.length;
+  }
+
+  removeItem(i){
+    this.items.splice(i,1);
   }
 
 }
