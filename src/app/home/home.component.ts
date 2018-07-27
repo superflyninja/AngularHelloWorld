89cda03baad9f565c1316e74f1cbd940ffdfe533
Patrick Horgan <phn@ersl.ie>
2018-07-27 10:28:02 +0100

    Now with exit animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,13 +9,22 @@ import {trigger,style,transition,animate,keyframes,query,stagger} from '@angular
     trigger('items', [
       transition('* => *', [
         query(':enter',style({opacity: 0}),{optional: true}),
+
         query(':enter',stagger('300ms',[
           animate('.6s ease-in',keyframes([
             style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
             style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
             style({opacity: 1, transform: 'translateY(0)', offset: 1}),
           ]))
-        ]),{optional: true})
+        ]),{optional: true}),
+
+        query(':leave',stagger('300ms',[
+          animate('.6s ease-in',keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 0, transform: 'translateY(-75px)', offset: 1}),
+          ]))
+        ]),{optional: true}),
       ])
     ])
   ]
